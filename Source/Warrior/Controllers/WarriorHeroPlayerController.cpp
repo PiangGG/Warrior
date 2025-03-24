81480136cbd235d20af5062793effd3116d6aadf
PiangGG <974805475@qq.com>
2025-03-24 23:41:27 +0800

    2.2 -Basic Class Structure

---
+++ Source/Warrior/Controllers/WarriorHeroPlayerController.cpp
@@ -0,0 +1,5 @@
+// Piang
+
+
+#include "WarriorHeroPlayerController.h"
+
---
+++ Source/Warrior/Controllers/WarriorHeroPlayerController.h
@@ -0,0 +1,17 @@
+// Piang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "WarriorHeroPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API AWarriorHeroPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
