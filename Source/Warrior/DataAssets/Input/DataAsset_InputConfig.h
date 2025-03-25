2549d947131288236eb5808f54ae4d4b60118372
PiangGG <974805475@qq.com>
2025-03-25 23:53:03 +0800

    2.6 -Input Config Data Asset

---
+++ Source/Warrior/DataAssets/Input/DataAsset_InputConfig.h
@@ -0,0 +1,42 @@
+// Piang
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "DataAsset_InputConfig.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+
+USTRUCT(BlueprintType)
+struct FWarriorInputActionConfig
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly,meta=(Category="Input"))
+	FGameplayTag InputTag;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputAction* InputAction;
+};
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputMappingContext * DefaultMappingContext;
+
+	/*
+	 * 在使用结构作为数组元素时，可通过自定义的格式来美化分组显示的标题。
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly,meta = (TitleProperty = "InputTag"))
+	TArray<FWarriorInputActionConfig> NativeInputActions;
+	
+	UInputAction* FindNativeInputActionByTag(const FGameplayTag& InInputTag);
+};
