714938fea77dafbf501f452c754e9dcd3b430d02
PiangGG <974805475@qq.com>
2025-04-23 00:22:05 +0800

    2.14 -Set Up Weapon Class

---
+++ Source/Warrior/Items/Weapons/WarriorHeroWeapon.cpp
@@ -0,0 +1,5 @@
+// Piang
+
+
+#include "WarriorHeroWeapon.h"
+
---
+++ Source/Warrior/Items/Weapons/WarriorWeaponBase.cpp
@@ -0,0 +1,22 @@
+// Piang
+
+
+#include "WarriorWeaponBase.h"
+
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AWarriorWeaponBase::AWarriorWeaponBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	SetRootComponent(WeaponMesh);
+	
+	WeaponCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("WeaponCollisionBox"));
+	WeaponCollisionBox->SetupAttachment(RootComponent);
+	WeaponCollisionBox->SetBoxExtent(FVector(20.0));
+	WeaponCollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
