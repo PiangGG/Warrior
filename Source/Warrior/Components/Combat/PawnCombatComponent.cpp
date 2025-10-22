d7f2e0c526799cd68b0e57f3e9a2ab835dbc2edd
PiangGG <974805475@qq.com>
2025-10-22 11:56:34 +0800

    3.24 -Section Wrap Up

--- Source/Warrior/Components/Combat/PawnCombatComponent.cpp
+++ Source/Warrior/Components/Combat/PawnCombatComponent.cpp
@@ -18,9 +18,6 @@ void UPawnCombatComponent::RegisterSpawnedWeapon(FGameplayTag InWeaponTagToRegis
 	{
 		CurrentEquippedWeaponTag = InWeaponTagToRegister;
 	}
-
-	const FString WeaponString = FString::Printf(TEXT("A Weapon named: %s has been registered using the tag %s"),*InWeaponToRegister->GetName(),*CurrentEquippedWeaponTag.ToString());
-	Debug::Print(WeaponString);
 }
 
 AWarriorWeaponBase* UPawnCombatComponent::GetCharacterCarriedWeaponByTag(FGameplayTag InWeaponTagToGet) const
--- Source/Warrior/WarriorTypes/WarriorStructTypes.h
+++ Source/Warrior/WarriorTypes/WarriorStructTypes.h
@@ -5,7 +5,7 @@
 #include "WarriorStructTypes.generated.h"
 
 class UInputMappingContext;
-class UWarriorGameplayAbility;
+class UWarriorHeroGameplayAbility;
 class UWarriorHeroLinkedAnimLayer;
 
 USTRUCT(BlueprintType)
@@ -17,7 +17,7 @@ struct FWarriorHeroAbilitySet
 	FGameplayTag InputTag;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	TSubclassOf<UWarriorGameplayAbility> AbilityToGrant;
+	TSubclassOf<UWarriorHeroGameplayAbility> AbilityToGrant;
 
 	bool IsValid() const;
 };
